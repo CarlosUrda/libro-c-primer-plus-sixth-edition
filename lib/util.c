02d638f4af19d955962393fb2d880940402ec8d7
Carlos A. Gómez Urda <carlosurda@yahoo.es>
2017-07-09 00:10:18 +0200

    Arreglar las tabulacones

--- lib/util.c
+++ lib/util.c
@@ -12,67 +12,67 @@
 * posible a ser leídos.
 *
 * Argumentos:
-* 	char * cadena: lugar donde guardar la cadena leída.
-* 	int numMax: valor para determinar el número máximo de caracteres a leer 
-* 		desde la entrada. El número máximo de caracteres a ser leídos es numMax-1.
+*   char * cadena: lugar donde guardar la cadena leída.
+*   int numMax: valor para determinar el número máximo de caracteres a leer 
+*       desde entrada. El número máximo de caracteres a ser leídos es numMax-1.
 *
 * Retorno:
-* 	Devuelve char * con la dirección donde se guarda la cadena leída si no hay 
-* 		error. Al final de la cadena leída añade un carácter fin de cadena '\0'.
-* 	Null si hay error (en tal caso el contenido de cadena es inconsistente). 
+*   Devuelve char * con la dirección donde se guarda la cadena leída si no hay 
+*       error. Al final de la cadena leída añade un carácter fin de cadena '\0'.
+*   Null si hay error (en tal caso el contenido de cadena es inconsistente). 
 */
 char * getstdin(char * cadena, int numMax)
 {
-	if (numMax < 1 || cadena == NULL)	
-	{
-		setUtilErr(UT_ERR_ARGS);
-		return NULL;
-	}
+    if (numMax < 1 || cadena == NULL)   
+    {
+        setUtilErr(UT_ERR_ARGS);
+        return NULL;
+    }
 
-	char caracter;
-	int i;
+    char caracter;
+    int i;
 
-	// Leer la cadena desde stdin
-	for (i = 0; i < numMax-1; ++i)
-	{
-		caracter = getchar();
+    // Leer la cadena desde stdin
+    for (i = 0; i < numMax-1; ++i)
+    {
+        caracter = getchar();
 
-		if (caracter == '\n') break;
+        if (caracter == '\n') break;
 
-		if (caracter == EOF)
-		{
-			if (feof(stdin) != 0) break;
+        if (caracter == EOF)
+        {
+            if (feof(stdin) != 0) break;
 
-			#if __UTILERR_DEBUG__ == 1
-				perror("Error en getchar()");
-			#endif	
-			setUtilErr(UT_ERR_LECTURA_STDIN);
-			return NULL;
-		}
+            #if __UTILERR_DEBUG__ == 1
+                perror("Error en getchar()");
+            #endif  
+            setUtilErr(UT_ERR_LECTURA_STDIN);
+            return NULL;
+        }
 
-		cadena[i] = caracter;
-	}
+        cadena[i] = caracter;
+    }
 
-	cadena[i] = '\0';
+    cadena[i] = '\0';
 
-	// Limpiar búfer.
-	if (i == numMax-1)
-	{
-		while ((caracter = getchar()) != '\n')
-		{
-			if (caracter != EOF) continue;
+    // Limpiar búfer.
+    if (i == numMax-1)
+    {
+        while ((caracter = getchar()) != '\n')
+        {
+            if (caracter != EOF) continue;
 
-			if (feof(stdin) != 0) break;
-			
-			#if __UTILERR_DEBUG__ == 1
-				perror("Error en getchar()");
-			#endif	
-			setUtilErr(UT_ERR_LIMPIAR_BUFFER_STDIN);
-			return NULL;
-		}
-	}
+            if (feof(stdin) != 0) break;
+            
+            #if __UTILERR_DEBUG__ == 1
+                perror("Error en getchar()");
+            #endif  
+            setUtilErr(UT_ERR_LIMPIAR_BUFFER_STDIN);
+            return NULL;
+        }
+    }
 
-	return cadena;
+    return cadena;
 }
 
 
@@ -81,56 +81,56 @@ char * getstdin(char * cadena, int numMax)
 * Limpiar el búfer de entrada a bajo nivel de un flujo de entrada.
 *
 * Argumentos:
-* 	int fd: Descriptor de fichero a limpiar su búfer.
+*   int fd: Descriptor de fichero a limpiar su búfer.
 *
 * Retorno:
-* 	UT_OK si el búfer es limpiado correctamente.
-* 	UT_ERR si hay algún error. En utilErr queda grabado el código de error.
+*   UT_OK si el búfer es limpiado correctamente.
+*   UT_ERR si hay algún error. En utilErr queda grabado el código de error.
 */
 int fflushin(int fd)
 {
-	if (fd < 0)
-	{
-		setUtilErr(UT_ERR_ARGS);
-		return UT_ERR;
-	}
+    if (fd < 0)
+    {
+        setUtilErr(UT_ERR_ARGS);
+        return UT_ERR;
+    }
 
-	struct timeval timeout, savetime;
-	fd_set readfds, savefds;
-	int retorno, retselect;
-	unsigned char byte;
-	
-	timeout.tv_sec = 0;
-	timeout.tv_usec = 0;	
-	FD_ZERO(&readfds);
-	FD_SET(fd, &readfds);
-	
-	savefds = readfds;
-	savetime = timeout;
+    struct timeval timeout, savetime;
+    fd_set readfds, savefds;
+    int retorno, retselect;
+    unsigned char byte;
+    
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 0;    
+    FD_ZERO(&readfds);
+    FD_SET(fd, &readfds);
+    
+    savefds = readfds;
+    savetime = timeout;
 
-	do
-	{
-		retselect = select(fd+1, &readfds, NULL, NULL, &timeout);
-	
-		switch (retselect)
-		{
-			case 0:
-				retorno = UT_OK;
-				break;
-			case -1:
-				#if __UTILERR_DEBUG__ == 1
-					perror("Error en función select");
-				#endif
-				setUtilErr(UT_ERR_COMPROBAR_BUFER_BAJO);
-				retorno = UT_ERR;
-				break;
-			default:
-				read(fd, &byte, 1);
-				readfds = savefds;
-				timeout = savetime;
-				break;
-		}
-	} while (retselect > 0);
+    do
+    {
+        retselect = select(fd+1, &readfds, NULL, NULL, &timeout);
+    
+        switch (retselect)
+        {
+            case 0:
+                retorno = UT_OK;
+                break;
+            case -1:
+                #if __UTILERR_DEBUG__ == 1
+                    perror("Error en función select");
+                #endif
+                setUtilErr(UT_ERR_COMPROBAR_BUFER_BAJO);
+                retorno = UT_ERR;
+                break;
+            default:
+                read(fd, &byte, 1);
+                readfds = savefds;
+                timeout = savetime;
+                break;
+        }
+    } while (retselect > 0);
 
-	return retorno;
+    return retorno;
 }
