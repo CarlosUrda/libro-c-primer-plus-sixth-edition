d86d5cc42e8c6c91107cbde480575fcb62e1eeca
Carlos A. Gómez Urda <carlosurda@yahoo.es>
2018-04-19 21:29:02 +0200

    Completa ejercicio 01
    - Añade números sin signo.
    - Sustituye tabulaciones por espacios en blanco.

--- unidad03/ejercicio01.c
+++ unidad03/ejercicio01.c
@@ -8,13 +8,18 @@
 
 int main(void)
 {
-	int entero = INT_MAX;
-	double decimalMax = DBL_MAX;
-	double decimalMin = DBL_MIN;
-	
-	printf("Desbordamiento del entero %d por arriba: %d\n", entero, entero+1);	
-	printf("Desbordamiento del double %e por arriba: %e\n", decimalMax, decimalMax+1e300);	
-	printf("Desbordamiento de double %e por abajo: %e. => Número Subnormal\n", decimalMin, decimalMin-1);	
+    int enteroMax = INT_MAX;
+    int enteroMin = INT_MIN;
+    unsigned int natural = UINT_MAX; 
+    double decimalMax = DBL_MAX;
+    double decimalMin = DBL_MIN;
+    
+    printf("Desbordamiento del natural %u por arriba: %u\n", natural, natural+1);
+    printf("Desbordamiento del entero %d por arriba: %d\n", enteroMax, enteroMax+1);
+    printf("Desbordamiento del natural %u por abajo: %u\n", 0U, 0U-1);
+    printf("Desbordamiento del entero %d por abajo: %d\n", enteroMin, enteroMin-1);
+    printf("Desbordamiento del double %e por arriba: %e\n", decimalMax, decimalMax+1e300);
+    printf("Desbordamiento de double %e por abajo: %e. => Número Subnormal\n", decimalMin, decimalMin-10);
 
-	return 0;
+    return 0;
 }
